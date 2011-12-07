29afb0be9f748a77ba8c0cae2628a114f133d009
chiva <santireig@gmail.com>
2011-12-07 11:12:47 +0100

    platex: get analog plot reference from mainwindow handle

--- ui/analog.py
+++ ui/analog.py
@@ -11,8 +11,7 @@ HISTORY = 60
 
 class AnalogTab(object):
 
-    def __init__(self, mwHandle, plotHandle):
-        self.plot = plotHandle
+    def __init__(self, mwHandle):
         self.mw = mwHandle
         self.curves = {}
         self.data = {}
@@ -22,29 +21,31 @@ class AnalogTab(object):
                                'bar' : eval("self.mw.analogBar%d" % (i)),
                                'label' : eval("self.mw.lbAnalog%d" % (i)) })
         
-        self.plot.setAutoReplot(False)
-        self.plot.plotLayout().setAlignCanvasToScales(True)
+        self.mw.analogPlot.setAutoReplot(False)
+        self.mw.analogPlot.plotLayout().setAlignCanvasToScales(True)
         grid = QwtPlotGrid()
         grid.enableXMin(True)
         grid.enableYMin(True)
-        grid.attach(self.plot)
+        grid.attach(self.mw.analogPlot)
         grid.setPen(QPen(Qt.black, 0, Qt.DotLine))
         grid.setMinPen(QPen(Qt.lightGray, 0, Qt.DotLine))
-        self.plot.setAxisScale(QwtPlot.xBottom, 0, HISTORY)
-        self.plot.setAxisScale(QwtPlot.yLeft, 0, 5)
+        self.mw.analogPlot.setAxisScale(QwtPlot.xBottom, 0, HISTORY)
+        self.mw.analogPlot.setAxisScale(QwtPlot.yLeft, 0, 5)
         
         self.mw.cbUnit.insertItems(0, ("Voltios", "Cuentas"))
+        self.mw.board.updateAnalog.connect(self.update)
+        self.mw.cbUnit.currentIndexChanged.connect(self.changedUnits)
         
         colors = (Qt.blue, Qt.red, Qt.black, Qt.darkGreen, Qt.darkCyan, Qt.magenta)
         for i in xrange(0, 6):
             curve = QwtPlotCurve()
             curve.setPen(QPen(colors[i], 2))
-            curve.attach(self.plot)
+            curve.attach(self.mw.analogPlot)
             self.curves[i] = curve
             self.data[i] = self._zeros(HISTORY+1)
             self.bars[i]['bar'].setFillBrush(colors[i])
         
-        self.plot.replot()
+        self.mw.analogPlot.replot()
 
     @pyqtSlot(int, int)
     def update(self, channel, value):
@@ -55,7 +56,7 @@ class AnalogTab(object):
         self.curves[channel].setData(range(0, HISTORY+1), self.data[channel])
         self.bars[channel]['bar'].setValue(value)
         self.bars[channel]['label'].setText(str(value))
-        self.plot.replot()
+        self.mw.analogPlot.replot()
 
     def enterTab(self):
         logger.debug("Entering analog tab")
@@ -71,8 +72,8 @@ class AnalogTab(object):
                     self.curves[channel].setVisible(False)
                     self.bars[channel]['group'].setEnabled(False)
                     self.bars[channel]['bar'].setValue(0)
-                    self.bars['label'].setText(str(0))
-        self.plot.replot()
+                    self.bars[channel]['label'].setText(str(0))
+        self.mw.analogPlot.replot()
 
     def exitTab(self):
         logger.debug("Exiting analog tab")
@@ -92,11 +93,11 @@ class AnalogTab(object):
     @pyqtSlot(int)
     def changedUnits(self, index):
         if index is 0:
-            self.plot.setAxisScale(QwtPlot.yLeft, 0, 5)
+            self.mw.analogPlot.setAxisScale(QwtPlot.yLeft, 0, 5)
             for bar in self.bars:
                 bar['bar'].setRange(0, 5)
         elif index is 1:
-            self.plot.setAxisScale(QwtPlot.yLeft, 0, 1024)
+            self.mw.analogPlot.setAxisScale(QwtPlot.yLeft, 0, 1024)
             for bar in self.bars:
                 bar['bar'].setRange(0, 1024)
-        self.plot.replot()
+        self.mw.analogPlot.replot()
