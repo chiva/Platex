2f34e6cd28ec51274fac31c3232805cf0a1ddf0a
chiva <santireig@gmail.com>
2011-12-08 19:17:06 +0100

    platex: added units selection por pwm values

--- ui/pwm.py
+++ ui/pwm.py
@@ -10,21 +10,52 @@ class PWMTab(object):
 
     def __init__(self, mwHandle):
         self.mw = mwHandle
+        self.sliders = list()
         
         for x in self.mw.board.pins:
             if x.PWM_CAPABLE:
-                eval("self.mw.pwmBar%02d" % x.pin_number).valueChanged.connect(self.updatePwm)
+                slider = eval("self.mw.pwmBar%02d" % x.pin_number)
+                slider.valueChanged.connect(self.updatePwm)
+                self.sliders.append(slider)
+                
+        self.mw.cbPwmUnit.insertItems(0, ("Porcentaje", "Cuentas"))
+        self.mw.cbPwmUnit.currentIndexChanged.connect(self.changedUnits)
 
     @pyqtSlot(int)
     def updatePwm(self, value):
-        number = int(self.mw.sender().property("objectName").toString()[-2:])
-        logger.debug("Pwm value pin "+str(number)+" set to "+str(int(value))+"%")
-        self.mw.board.pins[number].write(value/100)
+        if self.mw.cbPwmUnit.currentIndex() is 0:
+            number = int(self.mw.sender().property("objectName").toString()[-2:])
+            logger.debug("PWM value pin "+str(number)+" set to "+str(int(value))+"%")
+            self.mw.board.pins[number].write(value/100)
+        else:
+            number = int(self.mw.sender().property("objectName").toString()[-2:])
+            logger.debug("PWM value pin "+str(number)+" set to "+str(int(value)))
+            self.mw.board.pins[number].write(round(value/1023, 2))
 
     def enterTab(self):
         logger.debug("Entering PWM tab")
         for x in self.mw.board.pins:
-            eval("self.mw.gbPwm%02d" % x.pin_number).setEnabled(x.mode is 3)
+            if x.PWM_CAPABLE:            
+                eval("self.mw.gbPwm%02d" % x.pin_number).setEnabled(x.mode is 3)
 
     def exitTab(self):
         logger.debug("Exiting digital tab")
+
+    @pyqtSlot(int)
+    def changedUnits(self, index):
+        if index is 0:
+            logger.debug("Changed PWM units to percentage")
+            for slider in self.sliders:
+                value = slider.value()
+                slider.blockSignals(True)
+                slider.setRange(0, 100, 1)
+                slider.blockSignals(False)
+                slider.setValue(round(value*100/1023))
+        else:
+            logger.debug("Changed PWM units to counts")
+            for slider in self.sliders:
+                value = slider.value()
+                slider.blockSignals(True)
+                slider.setRange(0, 1023, 1)
+                slider.blockSignals(False)
+                slider.setValue(round(value*1023/100))
